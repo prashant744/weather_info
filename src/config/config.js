87c54b50859e565d7d11ef160db4a7aa60d59a74
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-31 15:22:27 +0530

    update functions

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+/**
+ * @description
+ * Get your openweathermap.org api key and paste it into the .env file.
+ * If you don't see an .env file, then rename sample.env to .env
+ * and follow the instructions there.
+ * @link https://home.openweathermap.org/api_keys
+ */
+const OPEN_WEATHER_MAP_API_KEY = process.env.REACT_APP_API_KEY;
+
+const config = {
+  WEATHER_API_ENDPOINT: `https://api.openweathermap.org/data/2.5/weather?appid=${OPEN_WEATHER_MAP_API_KEY}&`,
+  WEATHER_DATA_ENDPOINT: `https://api.openweathermap.org/data/2.5/onecall?appid=${OPEN_WEATHER_MAP_API_KEY}&exclude=minutely&units=metric&`,
+};
+
+export default config;
