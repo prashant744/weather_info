c1b6baadef2a63e0c917818e8b9910f54c2e6e16
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-31 10:43:15 +0530

    add pakages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import Temp from "./component/Temp";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Temp />
+    </>
   );
 }
 
